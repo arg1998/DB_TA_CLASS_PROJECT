3985407e7424cc1253ffda02c30df1f3ae00a3f4
arg1998 <arg7971@gmail.com>
2019-06-01 20:15:36 +0430

    updated the config file for SERVER_PORT

--- config/config.js
+++ config/config.js
@@ -3,18 +3,26 @@ let config = {};
 if (process.env.DEV) {
   config = require("./private");
 } else {
-  const { DB_USERNAME, DB_NAME, DB_PASS, DB_PORT, DB_URL } = process.env;
-  if (DB_NAME && DB_USERNAME && DB_PASS && DB_URL && DB_PORT) {
+  const {
+    DB_USERNAME,
+    DB_NAME,
+    DB_PASS,
+    DB_PORT,
+    DB_URL,
+    SERVER_PORT
+  } = process.env;
+  if (DB_NAME && DB_USERNAME && DB_PASS && DB_URL && DB_PORT && SERVER_PORT) {
     config = {
       db_pass: DB_PASS,
       db_username: DB_USERNAME,
       db_url: DB_URL,
       db_port: Number(DB_PORT),
-      db_name: DB_NAME
+      db_name: DB_NAME,
+      server_port: SERVER_PORT
     };
   } else {
     throw new Error(
-      "MUST Enter Environment Variables Correctly =>  DB_USERNAME, DB_NAME, DB_PASS, DB_PORT and DB_URL "
+      "MUST Enter Environment Variables Correctly =>  DB_USERNAME, DB_NAME, DB_PASS, DB_PORT, DB_URL and SERVER_PORT"
     );
   }
 }
