3c8d3d2cdd51082ea95175c566b43f76a79c0651
arg1998 <arg7971@gmail.com>
2019-06-01 20:15:18 +0430

    added user routes and some functionalites

---
+++ routes/User.route.js
@@ -0,0 +1,60 @@
+const UserModel = require("../models/User.model");
+const mongoose = require("mongoose");
+
+const registerUser = async (req, res) => {
+  const { name, username, password, email, date } = req.body;
+  const newUser = new UserModel({
+    name,
+    username,
+    password,
+    email,
+    birth_date: date
+  });
+
+  const result = await newUser.save();
+  if (result) {
+    res.send({ error: null, saved_user: result });
+  } else {
+    res.send({ error: "can not save the user" });
+  }
+};
+
+const addFriend = async (req, res) => {
+  const { user_id, friend_id } = req.body;
+  if (user_id === friend_id) {
+    res.send({ error: "IDs must not be identical" });
+    return;
+  }
+  let user = await UserModel.findById(user_id);
+  if (user) {
+    user.friends.push(new mongoose.Types.ObjectId(friend_id));
+    user = await user.save();
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ error: "couldn't add friend to this user" });
+    }
+  } else {
+    res.send({ error: "failed to add friend" });
+  }
+};
+
+const getUser = async (req, res) => {
+  const { user_id } = req.params;
+  if (user_id) {
+    const user = await UserModel.findById(user_id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({ error: "user not found" });
+    }
+  } else {
+    res.send({ error: "invalid user id" });
+  }
+};
+
+module.exports = {
+  registerUser,
+  getUser,
+  addFriend
+};
